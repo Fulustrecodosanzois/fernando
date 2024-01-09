e441bfb9653bd19cdd4c7115120b90a1418a6a1c
Luciano Silva <luciano_jet@hotmail.com>
2024-01-09 18:03:52 -0300

    ajustado js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const meuForm = document.querySelector(".formulario");
+    const modal = document.querySelector(".modal");
+    const mensagemAviso = document.getElementById("mensagem-aviso");
+    const closeBtn = document.querySelector(".close");
 
     meuForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Evita o envio padrão do formulário
+        event.preventDefault();
 
-        // Obtém os valores dos campos do formulário
         const nome = document.getElementById("nome").value;
         const telefone = document.getElementById("telefone").value;
         const mensagem = document.getElementById("mensagem").value;
 
-        // Array para armazenar os nomes dos campos vazios
         let camposFaltando = [];
 
-        // Verifica se todos os campos estão preenchidos
         if (nome.trim() === '') {
             camposFaltando.push('Nome');
         }
@@ -24,23 +23,29 @@ document.addEventListener("DOMContentLoaded", function () {
             camposFaltando.push('Mensagem');
         }
 
-        // Se algum campo estiver vazio, exibe a mensagem indicando quais campos estão faltando
         if (camposFaltando.length > 0) {
             const camposFaltandoStr = camposFaltando.join(', ');
-            alert(`Por favor, preencha o(s) campo(s) seguinte(s): ${camposFaltandoStr}`);
-            return; // Impede o envio se algum campo estiver vazio
+            mensagemAviso.textContent = `Por favor, preencha o(s) campo(s) seguinte(s): ${camposFaltandoStr}`;
+            modal.style.display = "block";
+            return;
         }
 
-        // Se todos os campos estiverem preenchidos, cria a mensagem formatada
         const mensagemFormatada = `Olá!!!%0A%0A**Informações do Contato**%0A%0A*Nome:* ${nome}%0A%0A*Telefone:* ${telefone}%0A%0A*Mensagem:*%0A%0A${mensagem}`;
-
-        const numeroWhatsApp = "5598987189453"; // Substitua pelo número de telefone desejado
-
+        const numeroWhatsApp = "5598987189453";
         const urlWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${mensagemFormatada}`;
 
-        // Abre a janela do WhatsApp com a mensagem preenchida
         window.open(urlWhatsApp);
-
     });
 
+    // Fechar o modal quando clicar no botão de fechar (X)
+    closeBtn.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    // Fechar o modal se o usuário clicar fora dele
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
 });
